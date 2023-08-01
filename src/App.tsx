8b3a9203a874fad70667e09e7d9a2326c2a4f213
Morgan Hondros <morganhondros@Morgans-MBP.localdomain>
2023-08-01 16:59:15 -0400

    api called for adding mock appointments.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda} from '@syncfusion/ej2-react-schedule';
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel} from '@syncfusion/ej2-react-schedule';
+import { DataManager, JsonAdaptor, Query, WebApiAdaptor } from '@syncfusion/ej2-data';
 
 
 function App() {
+  const localData: EventSettingsModel = {
+    dataSource: [{
+      EndTime: new Date(2023, 0,11,6,30),
+      StartTime: new Date(2023, 0, 11,4,0)
+    }]
+  }
+  const remoteData = new DataManager({
+    url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData', 
+    adaptor: new WebApiAdaptor,
+    crossDomain: true
+  })
   return (
     <div className="App">
       <br /><br /><br/>
-      <ScheduleComponent currentView='Month'>
+      <ScheduleComponent currentView='Month' selectedDate={new Date(2017, 5, 5)} eventSettings={{dataSource: remoteData}}>
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
-
+         
       </ScheduleComponent>
       
     </div>
